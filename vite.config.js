a924950c99a82201e08c0cf90d0becd78c1578a1
alcuidox <alex.currie@idoxgroup.com>
2026-03-19 15:49:09 +0000

    FIX: Typo in tokens path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+// import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@tokens' : resolve(--dirname, './dist'),
+      '@tokens' : resolve(__dirname, './dist'),
     },
   },
 });
